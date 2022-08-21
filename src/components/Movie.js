ae6820a7e7ce1d3c57af44aa825471573bb30f3d
hyunjin <phj97211@naver.com>
2022-08-21 12:40:07 +0900

    FEAT: 상단메뉴추가, 버튼추가, 목록배치방법 수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <div className="movie">
+    <Link
+      to={{
+        pathname: `/movie/${id}`,
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres
+        }
+      }}
+    >
+    
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
@@ -16,11 +30,13 @@ function Movie({ year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
+      </Link>
     </div>
   );
 }
+
 Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
---
+++ src/routes/Detail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Detail extends React.Component {
+    componentDidMount() {
+      const { location, history } = this.props;
+      if (location.state === undefined) {
+        history.push("/");
+      }
+    }
+}
+export default Detail;
